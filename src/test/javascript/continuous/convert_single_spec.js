3d6ac491840b73689e3c2dee016c6cfb672b57ea
moulliet <gmoulliet@flightstats.com>
2018-01-09 12:40:39 -0800

    changing script to revert storage switch

--- src/test/javascript/continuous/convert_single_spec.js
+++ src/test/javascript/continuous/convert_single_spec.js
@@ -7,7 +7,7 @@ hubUrl = 'http://' + hubUrl + '/channel';
 console.log('hubUrl:' + hubUrl);
 
 var timeout = 5 * 60 * 1000;
-var testPercent = parseInt(process.env.testPercent) || 10;
+var testPercent = parseInt(process.env.testPercent) || 20;
 console.log('testPercent: ' + testPercent);
 
 /**
@@ -19,14 +19,20 @@ console.log('testPercent: ' + testPercent);
  */
 describe(testName, function () {
 
-    var channels = [];
-    var converting = [];
+    /*const storageSource = 'SINGLE';
+    const storageDestination = 'BOTH';*/
+
+    const storageSource = 'BOTH';
+    const storageDestination = 'SINGLE';
+
+    let channels = [];
+    let converting = [];
 
     it('loads channels', function (done) {
         let headers = {'Accept': 'application/json'};
         utils.httpGet(hubUrl, headers)
             .then(res => {
-                var allChannels = res.body._links.channels;
+                let allChannels = res.body._links.channels;
                 allChannels.forEach(function (channel) {
                     console.log('channel', channel);
                     if (channel.name.substring(0, 4).toLowerCase() !== 'test') {
@@ -48,8 +54,8 @@ describe(testName, function () {
                 utils.httpGet(channel.href, headers)
                     .then(res => {
                         channel.storage = res.body.storage;
-                        if (channel.storage === "SINGLE") {
-                            console.log('single ', channel);
+                        if (channel.storage === storageSource) {
+                            console.log(storageSource + ' ', channel);
                             if (Math.random() * 100 <= testPercent) {
                                 console.log('converting ', channel);
                                 converting.push(channel);
@@ -67,17 +73,17 @@ describe(testName, function () {
 
     }, timeout);
 
-    it('changes SINGLE to BOTH', function (done) {
-        async.eachLimit(converting, 10,
+    it('changes ' + storageSource + ' to ' + storageDestination, function (done) {
+        async.eachLimit(converting, 20,
             function (channel, callback) {
 
                 console.log('switching to BOTH ', channel);
                 let url = `${channel.href}`;
                 let headers = {'Accept': 'application/json'};
-                let body = {storage: 'BOTH'};
+                let body = {storage: storageDestination};
                 utils.httpPut(url, headers, body)
                     .then(res => {
-                        console.log('success!  BOTH -> ', res.body);
+                        console.log('success!  -> ', res.body);
                         callback();
                     })
                     .catch(error => {
